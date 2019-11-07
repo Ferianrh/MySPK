a83cdc1c2d10a90209e0e8f42ca8cace957b5412
Ferianrh <fastercorner@gmail.com>
2019-11-07 23:18:03 +0700

    hapus navbar & ads

--- layout/menu_contents/content.php
+++ layout/menu_contents/content.php
@@ -1,20 +1,6 @@
 <!-- partial -->
 <div class="main-panel">
           <div class="content-wrapper">
-            <div class="row purchace-popup">
-              <div class="col-12 stretch-card grid-margin">
-                <div class="card card-secondary">
-                  <span class="card-body d-lg-flex align-items-center">
-                    <p class="mb-lg-0">Like what you see? Check out our premium version for more.</p>
-                    <a href="https://github.com/BootstrapDash/StellarAdmin-Free-Bootstrap-Admin-Template" target="_blank" class="btn ml-lg-auto download-button btn-success btn-sm my-1 my-sm-0">Download Free Version</a>
-                    <a href="https://www.bootstrapdash.com/product/stellar-admin/" target="_blank" class="btn btn-warning purchase-button btn-sm ml-sm-2 my-1 my-sm-0">Upgrade To Pro</a>
-                    <button class="close popup-dismiss ml-2">
-                      <span aria-hidden="true">&times;</span>
-                    </button>
-                  </span>
-                </div>
-              </div>
-            </div>
             <div class="row">
               <div class="col-md-4 grid-margin stretch-card">
                 <div class="card">
