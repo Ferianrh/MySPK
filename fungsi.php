7baa7d8032923ba01e7e706ad49f94d6430f3619
Ferianrh <fastercorner@gmail.com>
2020-02-09 16:11:36 +0700

    edit fungsi.php

--- fungsi.php
+++ fungsi.php
@@ -180,27 +180,11 @@ function viewPenduduk(){
 				echo '<td>'.$row[12].'</td>';
 				echo '<td>'.$row[13].'</td>';
 			}
-				
-				
-
 			?>
           </tr>
           
         </tbody>
       </table>
-    </div>
-    <div class="d-flex mt-4 flex-wrap">
-      <p class="text-muted">Showing 1 to 10 of 57 entries</p>
-      <nav class="ml-auto">
-        <ul class="pagination separated pagination-info">
-          <li class="page-item"><a href="#" class="page-link"><i class="icon-arrow-left"></i></a></li>
-          <li class="page-item active"><a href="#" class="page-link">1</a></li>
-          <li class="page-item"><a href="#" class="page-link">2</a></li>
-          <li class="page-item"><a href="#" class="page-link">3</a></li>
-          <li class="page-item"><a href="#" class="page-link">4</a></li>
-          <li class="page-item"><a href="#" class="page-link"><i class="icon-arrow-right"></i></a></li>
-        </ul>
-      </nav>
     </div>
       </div>
   </div>
